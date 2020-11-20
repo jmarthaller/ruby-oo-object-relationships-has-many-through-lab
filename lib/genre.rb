26f6da5b7c7d441194a24d1a9a1b0e3db5b8b670
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-19 21:13:42 -0500

    has-many-through interface

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
+require 'pry'
+
+class Genre
+    attr_accessor :name, :songs
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select { |song| song.genre == self }
+    end
+
+    def artists
+        Song.all.map { |song| song.artist }
+    end
+
+
+end
 
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,36 @@
+require 'pry'
 
+
+class Patient
+    attr_accessor :name 
+
+    attr_reader :date, :doctor 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self }
+    end
+
+    def doctors
+        Appointment.all.map { |appointment| appointment.doctor }
+    end
+    
+
+
+    
+
+end
